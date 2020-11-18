c0526d675e668cbf6c6a3a3f288d0a73ee342a21
CarloBredius <c.bredius@live.nl>
2020-11-18 13:58:17 +0100

    add obj loader and fps counter

--- src/game.cpp
+++ src/game.cpp
@@ -1,7 +1,12 @@
 #include "precomp.h" // include (only) this in every .cpp file
 #include "game.h"
 #include "ray.h"
+#include <chrono>
 
+// .obj loader
+#define TINYOBJLOADER_IMPLEMENTATION // define this in only *one* .cc
+#include "tiny_obj_loader.h"
+using namespace std;
 // -----------------------------------------------------------
 // Initialize the application
 // -----------------------------------------------------------
@@ -12,6 +17,24 @@ void Game::Init()
 	sphere = new Sphere(vec3(0, 0, 10), 3, 0xff0000);
 	floor = new Plane(vec3(0, 1, 0), 2, 0xffffff);
 	trian = new Triangle(vec3(0, 0, 15), vec3(4, 5, 12), vec3(6, -6, 13), 0x0000ff);
+
+	// load model
+	string inputfile = "assets/cube.obj";
+	tinyobj::attrib_t attrib;
+	vector<tinyobj::shape_t> shapes;
+	vector<tinyobj::material_t> materials;
+	string warn;
+	string err;
+
+	bool ret = tinyobj::LoadObj( &attrib, &shapes, &materials, &warn, &err, inputfile.c_str() );
+
+	if ( !warn.empty() )
+		cout << warn << endl;
+	if ( !err.empty() )
+		cerr << err << endl;
+	if ( !ret )
+		exit( 1 );
+
 }
 
 // -----------------------------------------------------------
@@ -27,6 +50,7 @@ void Game::Shutdown()
 // -----------------------------------------------------------
 void Game::Tick( float deltaTime )
 {
+	auto start = chrono::steady_clock::now();
 	printf("Game Tick\n");
 
 	vec3 p0 = view->TopLeft();
@@ -49,6 +73,8 @@ void Game::Tick( float deltaTime )
 		buf++;
 	}
 
+	chrono::duration<double> elapsed_seconds = chrono::steady_clock::now() - start;
+
 	// Write debug output
 	char buffer [32];
 	sprintf (buffer, "Pos: %f %f %f", view->position.x, view->position.y, view->position.z);
@@ -56,4 +82,8 @@ void Game::Tick( float deltaTime )
 
 	sprintf (buffer, "Dir: %f %f %f", view->direction.x, view->direction.y, view->direction.z);
 	screen->Print(buffer, 2, 9, 0xffff00);
+
+	// Timer
+	sprintf( buffer, "FPS: %f", 1 / elapsed_seconds.count());
+	screen->Print( buffer, 2, 16, 0xffff00 );
 }
