7da4d642235552e5c78619d392192909984e8bdc
Brian Janssen <brian-janssen@hotmail.com>
2020-12-12 14:47:18 +0100

    Micro optimise the timer elapsed function

--- src/timer.h
+++ src/timer.h
@@ -14,7 +14,7 @@ struct timer
 	{
 		auto diff = get() - start;
 		auto duration_us = std::chrono::duration_cast<MicroSeconds>( diff );
-		return static_cast<float>( duration_us.count() ) / 1000.0f;
+		return static_cast<float>( duration_us.count() ) * 0.001f;
 	}
 	static inline TimePoint get()
 	{
