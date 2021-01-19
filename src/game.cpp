a4d03d2dfee7ae0708f1e548068de46085fbc78c
Brian Janssen <test>
2021-01-19 17:23:38 +0100

    Use a 9x9 guassian kernel

--- src/game.cpp
+++ src/game.cpp
@@ -435,18 +435,13 @@ Color Game::Sample(Ray r, bool specularRay, uint depth, uint pixelId)
 	return result;
 }
 
-// Generated with: http://dev.theomader.com/gaussian-kernel-calculator/
-// kernel_size MUST be an odd number.
-const size_t kernel_size = 3;
-const size_t kernel_center = kernel_size / 2;
-const float kernel[kernel_size * kernel_size] = {
-    0.077847, 0.123317, 0.077847,
-    0.123317, 0.195346, 0.123317,
-    0.077847, 0.123317, 0.077847,
-};
-
 Color Game::Filter( uint pixelId )
 {
+	const float kernel[3 * 3] = {
+		0.077847, 0.123317, 0.077847,
+		0.123317, 0.195346, 0.123317,
+		0.077847, 0.123317, 0.077847,
+	};
 	Color tmp = Color( 0, 0, 0 );
 	tmp += kernel[0] * pixelData[pixelId - 1 - screen->GetWidth()].color;
 	tmp += kernel[1] * pixelData[pixelId     - screen->GetWidth()].color;
@@ -459,6 +454,22 @@ Color Game::Filter( uint pixelId )
 	tmp += kernel[8] * pixelData[pixelId + 1 + screen->GetWidth()].color;
 	return tmp;
 }
+
+// Generated with: http://dev.theomader.com/gaussian-kernel-calculator/
+// kernel_size MUST be an odd number.
+const size_t kernel_size = 9;
+const size_t kernel_center = kernel_size / 2;
+const float kernel[kernel_size * kernel_size] = {
+	0, 0.000001, 0.000014, 0.000055, 0.000088, 0.000055, 0.000014, 0.000001, 0,
+	0.000001, 0.000036, 0.000362, 0.001445, 0.002289, 0.001445, 0.000362, 0.000036, 0.000001,
+	0.000014, 0.000362, 0.003672, 0.014648, 0.023205, 0.014648, 0.003672, 0.000362, 0.000014,
+	0.000055, 0.001445, 0.014648, 0.058434, 0.092566, 0.058434, 0.014648, 0.001445, 0.000055,
+	0.000088, 0.002289, 0.023205, 0.092566, 0.146634, 0.092566, 0.023205, 0.002289, 0.000088,
+	0.000055, 0.001445, 0.014648, 0.058434, 0.092566, 0.058434, 0.014648, 0.001445, 0.000055,
+	0.000014, 0.000362, 0.003672, 0.014648, 0.023205, 0.014648, 0.003672, 0.000362, 0.000014,
+	0.000001, 0.000036, 0.000362, 0.001445, 0.002289, 0.001445, 0.000362, 0.000036, 0.000001,
+	0, 0.000001, 0.000014, 0.000055, 0.000088, 0.000055, 0.000014, 0.000001, 0,
+};
 
 float ComputeWeightRaw(const float sigma, const float value)
 {
